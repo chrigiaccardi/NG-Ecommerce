f5ec16b9710fb5b82a3d1c2e0d47af757f1033fd
Gianni Marchiaro Automobili <giannimarchiaroautomobili@Mac-2.lan>
2026-04-16 07:40:16 +0200

    transizioni card interattive

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHotToastConfig } from '@ngxpert/hot-toast';
@@ -7,8 +7,9 @@ import { provideHotToastConfig } from '@ngxpert/hot-toast';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    // inseriamo withComponentInputBinding per collegare automaticamente la route all'input
-    provideRouter(routes, withComponentInputBinding()),
+    // inseriamo withComponentInputBinding per collegare automaticamente la route all'input.
+    // withViewTransition è una API nativa di Angular che ci permette di impostare transizioni particolari ed interattive al progetto
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
     // configura tutti i toast globalmente con margin top, stacking depth (effetto 3D) e durata di 1 sec per UX veloce
     provideHotToastConfig({style: {marginTop: '70px'}, stacking: 'depth', duration: 1000}),
   ],
