c095331d9ecaf1ab1852adf10bc9b5ea22c8afaa
Gianni Marchiaro Automobili <giannimarchiaroautomobili@Mac-2.lan>
2026-04-17 22:16:34 +0200

    Aggiunta Angular SSR per Server Side Rendering

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter, withComponentInputBinding, withViewTransitions } from '@
 
 import { routes } from './app.routes';
 import { provideHotToastConfig } from '@ngxpert/hot-toast';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +12,7 @@ export const appConfig: ApplicationConfig = {
     // withViewTransition è una API nativa di Angular che ci permette di impostare transizioni particolari ed interattive al progetto
     provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
     // configura tutti i toast globalmente con margin top, stacking depth (effetto 3D) e durata di 1 sec per UX veloce
-    provideHotToastConfig({style: {marginTop: '70px'}, stacking: 'depth', duration: 1000}),
+    provideHotToastConfig({ style: { marginTop: '70px' }, stacking: 'depth', duration: 1000 }),
+    provideClientHydration(withEventReplay()),
   ],
 };
